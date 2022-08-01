27e18e2d251a2bd80e6e3ab87b2d9588e67fb4ce
hyelim3 <shl0703ss@gmail.com>
2022-08-01 22:56:31 +0900

    useEffect 실행

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Counter = ({ counter, setCounter }) => {
   const [searchWord, setSearchWord] = useState("");
+  const [data, setData] = useState([]);
   const addCounter = () => {
     setCounter(counter + 1);
   };
@@ -15,18 +17,30 @@ const Counter = ({ counter, setCounter }) => {
 
   useEffect(() => {
     //API 호출코드
-    console.log("API 호출");
-  }, []);
+    const getData = async () => {
+      const data = await axios.get(
+        "https://picsum.photos/v2/list?page=2&limit=100"
+      );
+      console.log(data);
+      //setData(data);
+    };
+    getData();
+  }, [data]); // 최초 1회 실행
 
   useEffect(() => {
     //API 호출코드와 같다.
     console.log("change input");
-  }, [searchWord]); //값이 변경될 때 콘솔이 실행되기를 바란다,
+  }, [searchWord]); // searchWord 값의 변화 감지
+  //값이 변경될 때 콘솔이 실행되기를 바란다,
   // 원하는 값의 변경만을 감지해서 내부의 동작을 실행
 
   useEffect(() => {
     console.log(counter);
-  }, [counter]); //counter변화할 때 어디까지 감지할건지..!
+  }, [counter]); // counter 값의 변화 감지
+
+  useEffect(() => {
+    console.log("뭔가변함");
+  }, [counter, searchWord]); // counter 값의 변화 감지
 
   return (
     <div>
