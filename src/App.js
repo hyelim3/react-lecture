42919c12c5b28934d7ae14cdb2961f39eef920f1
hyelim3 <shl0703ss@gmail.com>
2022-08-01 21:22:24 +0900

    useEffect deps

--- src/App.js
+++ src/App.js
@@ -4,51 +4,12 @@ import Counter from "./Components/Counter";
 import MyBtn from "./Components/MyBtn";
 
 function App() {
-  const [amount, setAmount] = useState(0);
-  const [active, setActive] = useState(true);
-  const reset = () => {
-    setAmount(0);
-  };
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  };
-  const changeActive = () => {
-    reset();
-    setActive(!active);
-  };
+  const [counter, setCounter] = useState(0);
 
   return (
-    <div>
-      <div>Time Converter</div>
-      <div>
-        Minutes{" "}
-        <input
-          placeholder="Minutes"
-          value={active ? amount : amount * 60}
-          type="number"
-          onChange={onChange}
-          disabled={!active}
-          //활성화 되어있을 때 active true -> 꺼지면 안되니깐 반전값을 넣어야함
-        />
-        <div>
-          Hours{" "}
-          <input
-            type="number"
-            placeholder="Hours"
-            value={active ? Math.floor(amount / 60) : amount}
-            //active 값이 true일 대 실행 비활성화되면 active true -> 비활성 amount 변환
-
-            disabled={active}
-            // active 최초의 거짓, !active
-            onChange={onChange}
-          />
-          <div>
-            <button onClick={reset}>reset</button>
-            <button onClick={changeActive}>active toggle</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Counter counter={counter} setCounter={setCounter} />
+    </>
   );
 }
 
--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,15 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const Counter = ({ counter, setCounter, name }) => {
+const Counter = ({ counter, setCounter }) => {
+  const [searchWord, setSearchWord] = useState("");
   const addCounter = () => {
     setCounter(counter + 1);
   };
+  const onChange = (e) => {
+    setSearchWord(e.target.value);
+  };
+
+  // console.log(counter);
+  //검색시 호출
+  //console.log(searchWord);
+
+  useEffect(() => {
+    //API 호출코드
+    console.log("API 호출");
+  }, []);
+
+  useEffect(() => {
+    //API 호출코드와 같다.
+    console.log("change input");
+  }, [searchWord]); //값이 변경될 때 콘솔이 실행되기를 바란다,
+  // 원하는 값의 변경만을 감지해서 내부의 동작을 실행
+
+  useEffect(() => {
+    console.log(counter);
+  }, [counter]); //counter변화할 때 어디까지 감지할건지..!
 
   return (
     <div>
-      <div>안녕하세요 {name}님</div>
+      <input
+        type="text"
+        onChange={onChange}
+        value={searchWord}
+        placeholder="검색어"
+      />
       <div>{counter}번 클릭했습니다</div>
-      <button onClick={addCounter}>Click me</button>
+      <button onClick={addCounter}>click</button>
     </div>
   );
   return;
